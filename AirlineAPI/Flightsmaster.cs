a867c65cdcf178d80201424a03d3fcc281eac8dc
PriyaJose08 <96871921+PriyaJose08@users.noreply.github.com>
2022-04-19 10:44:59 +0530

    Add files via upload

---
+++ AirlineAPI/Flightsmaster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineLibrary.Models
+{
+    public class Flightsmaster
+    {
+       [Key]
+        public int FlightregistrationId { get; set; }
+        public string? Flightname { get; set; }
+        public decimal? Economyseats { get; set; }
+        public decimal? Businessseats { get; set; }
+
+
+    }
+}
